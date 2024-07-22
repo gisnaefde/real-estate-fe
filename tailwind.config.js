70960bc4748738077d965161651e61b9b0153bd2
Gisna Fauzian Dermawan <gisnafauziande198@gmail.com>
2024-07-22 11:25:29 +0700

    create header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      green: '#27ae60',
+      gray:'#808080',
+      white:'#FFFFFF',
+    }
+  },
+  plugins: [],
+}
+
